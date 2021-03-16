1fdb7caaf0b7933914e476b906b118e49494c821
Kevin Collas-Arundell <kevin.ca@momenton.com.au>
2021-03-17 10:20:21 +1100

    remove duplicate retry and integrity check

--- db-update.sh
+++ db-update.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-MAX_RETRIES=5
 DATA_DIR="/clamavdb"
 
 function updatedb() {
 	/usr/bin/freshclam --datadir=$DATA_DIR --config-file=/etc/clamav/freshclam.conf
 }
 
-function check-db-integrity() {
- if [[ -f $DATA_DIR/bytecode.cld && -f $DATA_DIR/daily.cld && -f $DATA_DIR/main.cvd ]];then
-    echo "AV DB integrity check is fine"
-    return 0
- fi
- echo "AV DB integrity check has failed, some files are missing."
- return 1
-}
 
 for (( i=1; i<MAX_RETRIES; i++ ));
 do 
  echo "DB Update attempt no.: $i"
  updatedb
  EXIT_CODE=$?
- check-db-integrity
- INTEGRITY_CHECK=$?
- if [[ $EXIT_CODE -eq 0 && $INTEGRITY_CHECK -eq 0 ]]; then 
+ if [[ $EXIT_CODE -eq 0 ]]; then 
   echo  "DB updated succesfully"
   exit 0
  fi
