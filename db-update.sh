c7afe60733ecbea4a6a61b69475c3e32e3456135
Kevin Collas-Arundell <kevin.ca@momenton.com.au>
2021-03-17 10:22:52 +1100

    simplify update script

--- db-update.sh
+++ db-update.sh
@@ -2,22 +2,5 @@
 
 DATA_DIR="/clamavdb"
 
-function updatedb() {
-	/usr/bin/freshclam --datadir=$DATA_DIR --config-file=/etc/clamav/freshclam.conf
-}
+/usr/bin/freshclam --datadir=$DATA_DIR --config-file=/etc/clamav/freshclam.conf
 
-
-for (( i=1; i<MAX_RETRIES; i++ ));
-do 
- echo "DB Update attempt no.: $i"
- updatedb
- EXIT_CODE=$?
- if [[ $EXIT_CODE -eq 0 ]]; then 
-  echo  "DB updated succesfully"
-  exit 0
- fi
- sleep 60
-done ;
-
-echo "DB update failed"
-exit 1
